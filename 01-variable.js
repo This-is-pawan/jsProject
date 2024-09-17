21b5df2a091476125f4044960c6a42f237ab9810
pawan <pawanjalandhara@gmail.com>
2024-09-17 20:41:50 +0530

    jsUpdate

---
+++ 01-variable.js
@@ -0,0 +1,59 @@
+ //  case of var
+    var testing1=89; 
+    var testing1='High jump'
+     testing1=' javascript'
+    console.log(testing1);
+    console.log(testing1);
+    console.log(testing1);
+    //    in three case same string print because of over-ride 
+    //   in block scope    **var is not mostly useable in code due to create  bug
+    {
+        var block = 'This is js'
+        console.log(block);
+        
+    }
+     var bl = 'This is js'
+    // console.log(bl);
+    // console.log(block);
+
+
+
+
+
+ //   case of let 
+    let  letter='High jump'
+    // let  letter='High jump' not use same identifier with let
+    letter='890'
+    // console.log(letter);
+    // console.log(letter);
+     
+   
+    {
+        let blocks = 'This is js'
+        // console.log(blocks);
+        
+    }
+     let bs = "Hello"
+    // console.log(bs);
+    // console.log(blocks); not use in as a  global scope
+
+
+
+
+const sea1= 'variable';
+// const sea1= 'variable'; not use  this case 
+//  sea1= 'variable';
+//  console.log(sea1);
+{
+        sea3 = 'This is js'
+        // console.log(sea3);
+        
+    }
+// console.log(sea3); not work
+
+
+
+
+
+
+
