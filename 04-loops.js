a7c003b75b0a9921f3f003998bd37d53779344c9
pawan <pawanjalandhara@gmail.com>
2024-09-18 11:56:28 +0530

    forthfile Update

---
+++ 04-loops.js
@@ -0,0 +1,68 @@
+// loops 1)while
+// loops 1)do while
+// loops 1)for loop
+// loops 1)for of 
+// loops 1)for in
+// examples 
+
+// const firstLoop ={
+//     Author:'joan sk',
+//     book:'True story',
+    
+// }
+// let arr={
+//     num:90,
+//     num2:89
+// }
+// let i =0; 
+// while (i<arr.length ) {
+    
+//     const element = arr[i];
+//     i++
+// console.log(element);
+//     }
+let arr=1;
+let i=3;
+do{
+// console.log('do while loop');
+
+}while (i<arr)
+    
+    
+    
+
+
+
+
+let n //= prompt('set input ')
+let nu=Number.parseInt(n)
+let sum=0
+for (let i = 0; i < nu; i++) {
+sum +=Number.parseInt(i)
+//    console.log(`${i}`);
+//    console.log(`${sum}`);
+   
+    
+}
+const object ={
+        Author:'joan sk',
+        book:'True story',
+        
+    }
+// for (const key in objects) {
+//    console.log(key+' '+ objects[key]);
+   
+// }
+for (const k of 'happy') {
+    // console.log(k); array or string use with object
+    console.log(k);
+    
+ }
+ 
+
+
+
+
+
+
+
