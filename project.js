b3ad63c2f74c81566ef03968882065ad148a71f4
pawan <pawanjalandhara@gmail.com>
2024-09-27 12:21:43 +0530

    jsupdate

--- project.js
+++ project.js
@@ -1,85 +1,82 @@
-let bar=document.querySelector('.fa-chart-bar');
-let barList=document.querySelector('.barList');
-let dataTypes=document.querySelector('.dataTypes');
-let dataTypes2=document.querySelector('.dataTypes-2');
-let moon=document.querySelectorAll('.fa-moon');
-let popupShow=document.querySelector('.popup');
-let span=document.querySelector('.popup span');
-let dataTypesShow=document.querySelector('.dataTypesShow ul');
-
+let bar = document.querySelector(".fa-chart-bar");
+let barList = document.querySelector(".barList");
+let dataTypes = document.querySelector(".dataTypes");
+let dataTypes2 = document.querySelector(".dataTypes-2");
+let moon = document.querySelectorAll(".fa-moon");
+let popupShow = document.querySelector(".popup");
+let span = document.querySelector(".popup span");
+let dataTypesShow = document.querySelector(".dataTypesShow ul");
 
 // console.log(moon);
 
-let arr = ["A", "B", "C", "D", "E", "F", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-let joining = '#';
+let arr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+let joining = "#";
 
 let randomColor = () => {
-    joining = '#'; 
-    for (let i = 0; i < 6; i++) { 
-        let random = Math.floor(Math.random() * arr.length);
-        joining += arr[random];
-    }
-    return joining;
-}
-
+  joining = "#";
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * arr.length);
+    joining += arr[random];
+  }
+  return joining;
+};
 
 moon.forEach((e) => {
-    e.addEventListener('click', () => {
-        let newColor = randomColor(); 
-        // console.log(newColor);
-        setTimeout(() => {
-            
-            popupShow.classList.toggle('popupShow');
-            popupShow.style.background=newColor;
-        }, 200);
+  e.addEventListener("click", () => {
+    let newColor = randomColor();
+    // console.log(newColor);
+    let out = setTimeout(() => {
+      popupShow.classList.toggle("popupShow");
+      popupShow.style.background = newColor;
+    }, 200);
+    setTimeout(() => {
+      clearTimeout(out);
+      popupShow.classList.remove("popupShow");
+    }, 2000);
 
-        document.body.style.background = newColor; 
-        e.style.color=newColor;
-    });
+    document.body.style.background = newColor;
+    e.style.color = newColor;
+  });
 });
 
-
-
-let caretUp=document.querySelectorAll('.fa-square-caret-up');
+let caretUp = document.querySelectorAll(".fa-square-caret-up");
 // console.log(bar);
-bar.addEventListener('click',()=>{
-    barList.classList.toggle('barListShow');
-    bar.classList.toggle('barAnimate');
-})
-let tr='upRotate';
-caretUp.forEach((e)=>{
-    // let transfer=es
-    e.addEventListener('click',(value)=>{
-    value.target.style.color= randomColor();
-    
-    
-    // randomColor();
-    value.target.classList.toggle('upRotate')
-    
-    if (value.currentTarget.classList.contains('one')) {
-        // console.log('ok');
-        dataTypes.classList.toggle('dataTypesShow');   
-        
-        
-    } else if(value.currentTarget.classList.contains('two')) {
-        dataTypes2.classList.toggle('dataTypesShow2');   
-        // console.log('not ok');
-    }
-    
-})
-
-
+bar.addEventListener("click", () => {
+  barList.classList.toggle("barListShow");
+  bar.classList.toggle("barAnimate");
 });
+let tr = "upRotate";
+caretUp.forEach((e) => {
+  // let transfer=es
+  e.addEventListener("click", (value) => {
+    value.target.style.color = randomColor();
 
+    // randomColor();
+    value.target.classList.toggle("upRotate");
 
-
-
-
-
-
-
-
-
-
-
-
+    if (value.currentTarget.classList.contains("one")) {
+      // console.log('ok');
+      dataTypes.classList.toggle("dataTypesShow");
+    } else if (value.currentTarget.classList.contains("two")) {
+      dataTypes2.classList.toggle("dataTypesShow2");
+      // console.log('not ok');
+    }
+  });
+});
